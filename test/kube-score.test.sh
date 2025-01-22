3893a250d634b3773682f5b306e6417ab19c19fb
Abraham <kashuaabraham@outlook.com>
2025-01-22 17:47:27 +0100

    delete unnecessary files

--- test/kube-score.test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kube-score score path/to/your/kubernetes/manifests | tee kube-score-output.txt
-if grep -q "FAIL" kube-score-output.txt; then
-  echo "Kube-score found issues in the manifests."
-  exit 1
-else
-  echo "Kube-score passed without issues."
-  exit 0
-fi
